4f7ed9a6b282283999719fd689e2dc139a486aab
loumouli <louis.moulin@outlook.fr>
2024-06-07 19:52:58 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,50 @@
-use std::net::TcpListener;
-use std::thread;
+use std::{io, thread};
+use std::collections::HashMap;
+use std::error::Error;
+use std::io::ErrorKind::WouldBlock;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread::sleep;
+use std::time::Duration;
 
-use leb128;
+use lazy_static::lazy_static;
 
 use mpacket::Packet;
 
 mod mpacket;
 
+trait PacketHandler {
+    fn handle_packet(&self, client: &mut Client, packet: &Packet) -> Result<(), Box<dyn Error>>;
+}
+
+struct TargetHandler;
+impl PacketHandler for TargetHandler {
+    fn handle_packet(&self, client: &mut Client, packet: &Packet) -> Result<(), Box<dyn Error>> {
+        // Handle Target packets
+        println!("Target packet handler: {:?}", packet);
+        Ok(())
+    }
+}
+
+lazy_static! {
+    static ref HANDSHAKING_HANDLERS: HashMap<i64, Box<dyn PacketHandler>> = {
+        let mut m = HashMap::new();
+        m.insert(0, Box::new(HandshakingHandler));
+        // Add more handlers for different packet IDs
+        m
+    };
+
+    static ref TARGET_HANDLERS: HashMap<i64, Box<dyn PacketHandler>> = {
+        let mut m = HashMap::new();
+        m.insert(1, Box::new(TargetHandler));
+        // Add more handlers for different packet IDs
+        m
+    };
+
+    // Define other state handlers similarly...
+}
+
+#[derive(Debug, Eq, PartialEq)]
 enum StateClient {
     Handshaking,
     Target,
@@ -15,26 +53,79 @@ enum StateClient {
     Transfer,
 }
 
-struct client {
+
+#[derive(Debug)]
+struct Client {
     status: StateClient,
-    server_address: String,
-    server_port: u16,
-    protocol_version: i32,
+    tcp_stream: TcpStream,
+    addr: SocketAddr,
 }
 
-impl client {
-    pub fn process_packet(packet: Packet) {}
-}
 
-fn main() {
-    println!("Hello, world!");
-    let listener = TcpListener::bind("localhost:25565").unwrap();
-    println!("listener created {:?}", listener);
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        thread::spawn(move || {
-            let packet = Packet::new(&mut stream);
-            client::process_packet(packet)
-        });
+impl Client {
+    pub fn process_packet(&mut self, packet: &Packet) -> Result<(), Box<dyn Error>> {
+        match self.status {
+            StateClient::Handshaking => {}
+            StateClient::Target => {}
+            StateClient::Status => {}
+            StateClient::Login => {}
+            StateClient::Transfer => {}
+        }
+        Ok(())
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut handlers: HashMap<i64, Box<dyn PacketHandler>> = HashMap::new();
+    let clients_arc = Arc::new(Mutex::new(Vec::<Client>::new()));
+    let clients_arc_clone = Arc::clone(&clients_arc);
+    let listener = TcpListener::bind("localhost:25565").unwrap();
+    let client_thread = thread::spawn(move || {
+        loop {
+            let mut clients = clients_arc.lock().unwrap();
+            let mut bad_clients: Vec<usize> = Vec::new();
+            for (index, client) in clients.iter_mut().enumerate() {
+                let packet = match Packet::new(&mut client.tcp_stream) {
+                    Ok(pck) => pck,
+                    Err(e) => {
+                        if let Some(io_error) = e.downcast_ref::<io::Error>() {
+                            match io_error.kind() {
+                                WouldBlock => continue,
+                                _ => {}
+                            }
+                        }
+                        eprintln!("Error when crafting packet: {e}");
+                        bad_clients.push(index);
+                        continue;
+                    }
+                };
+                client.process_packet(&packet).unwrap()
+            }
+            for &pos in bad_clients.iter().rev() { //removing client  that give an error before
+                eprintln!("removing client from vec");
+                clients.remove(pos);
+            }
+            drop(clients);
+            sleep(Duration::from_millis(800));
+        }
+    });
+    loop {
+        let (tcp_stream, addr) = match listener.accept() {
+            Ok(res) => res,
+            Err(e) => {
+                eprintln!("Error when accepting: {}", e);
+                break;
+            }
+        };
+        println!("adding client ");
+        let client: Client = Client {
+            status: StateClient::Handshaking,
+            tcp_stream,
+            addr,
+        };
+        client.tcp_stream.set_nonblocking(true).unwrap();
+        clients_arc_clone.lock().unwrap().push(client);
+    }
+    client_thread.join().unwrap();
+    return Ok(());
 }
--- src/mpacket.rs
+++ src/mpacket.rs
@@ -1,6 +1,8 @@
+use std::error::Error;
 use std::io::Read;
 use std::net::TcpStream;
 
+#[derive(Debug)]
 pub struct Packet {
     length: i64,    //varInt
     packet_id: i64, //varInt
@@ -8,18 +10,16 @@ pub struct Packet {
 }
 
 impl Packet {
-    pub fn new(tcp_stream: &mut TcpStream) -> Packet {
-        let length = leb128::read::signed(tcp_stream).unwrap();
-        let packet_id = leb128::read::signed(tcp_stream).unwrap();
+    pub fn new(tcp_stream: &mut TcpStream) -> Result<Packet, Box<dyn Error>> {
+        let length = leb128::read::signed(tcp_stream)?;
+        let packet_id = leb128::read::signed(tcp_stream)?;
         let mut data = Vec::new();
         data.resize(length as usize, 0);
-        tcp_stream
-            .read_exact(&mut data[0..length as usize])
-            .unwrap();
-        Packet {
+        tcp_stream.read_exact(&mut data[0..length as usize])?;
+        Ok(Packet {
             length,
             packet_id,
             data,
-        }
+        })
     }
 }
